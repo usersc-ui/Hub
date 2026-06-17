a79c7734b1ab20cd89f69de329cd8795bbe2dd40
usersc-ui <clansofclansalt01@gmail.com>
2026-06-17 23:31:53 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const initialData = [
     {id: "p9", name: "Comatozze", link: "https://mega.nz/folder/hywlGJDY#dDp9Q3NFv_9cDkroIRlL4Q", img: "https://i.imgur.com/XZyrg68.jpeg"},
     {id: "p10", name: "ambergianna", link: "https://mega.nz/folder/OFljlCQa#Euxx9eB5S5uhDDPOCjNEOw", img: "https://i.imgur.com/7goIJ8U.jpeg"},
     {id: "p11_new", name: "creamyspot", link: "https://mega.nz/folder/FQtBHIoA#bMNIVyAAPrtinorDC0JeJA", img: "https://i.imgur.com/gI0CfNC.jpeg?t=" + new Date().getTime()}
-];
+];,
+{id: "p12", name: "gabbyblessings", link: "https://mega.nz/folder/uCxzFRgJ#Opwp5C5UzHMzmmtz_c2q5A", img: "https://i.imgur.com/9MafsCv.png"}
 
 const container = document.getElementById('container');
 container.innerHTML = ''; // Vorher leeren
