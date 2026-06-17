e0791e5d180aafb36aa976a4db936b567c006e09
usersc-ui <clansofclansalt01@gmail.com>
2026-06-17 08:10:08 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const initialData = [
     {id: "p8", name: "lexi marvel", link: "https://mega.nz/folder/bZM2VRAK#6wBv39rbrItW8YOdYMtjVQ", img: "https://i.imgur.com/XyFJ2Du.jpeg"},
     {id: "p9", name: "Comatozze", link: "https://mega.nz/folder/hywlGJDY#dDp9Q3NFv_9cDkroIRlL4Q", img: "https://i.imgur.com/XZyrg68.jpeg"},
     {id: "p10", name: "ambergianna", link: "https://mega.nz/folder/OFljlCQa#Euxx9eB5S5uhDDPOCjNEOw", img: "https://i.imgur.com/7goIJ8U.jpeg"},
-    {id: "p11", name: "creamyspot", link: "https://mega.nz/folder/FQtBHIoA#bMNIVyAAPrtinorDC0JeJA", img: "https://i.imgur.com/gI0CfNC.jpeg"}
+    // HIER GEÄNDERT: ID angepasst und Bild-URL mit Cache-Buster
+    {id: "p11_new", name: "creamyspot", link: "https://mega.nz/folder/FQtBHIoA#bMNIVyAAPrtinorDC0JeJA", img: "https://i.imgur.com/gI0CfNC.jpeg?t=" + new Date().getTime()}
 ];
+
 const container = document.getElementById('container');
 container.innerHTML = ''; // Vorher leeren
 
